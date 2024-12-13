6b3d0607fba2c0d4787ab6c587c4f1427fef6e24
남수린 <lover030114@gmail.com>
2024-12-13 21:22:16 +0900

    202213172남수린_수정함

--- stack.c
+++ stack.c
@@ -9,6 +9,7 @@ Stack * create_stack(int initial_size)
 	s->contents = (int *)malloc(initial_size*sizeof(Item));
 	s->top=0;
 	s->size=initial_size;
+	printf("스택이 만들어짐.\n");
 	return s;
 }
 
@@ -73,4 +74,5 @@ static void reallocate(Stack * stack)
 	free(stack->contents);
 	stack->contents = tmp;
 }
+
 
